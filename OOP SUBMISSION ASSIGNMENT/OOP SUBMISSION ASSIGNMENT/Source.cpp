dc5799a208baf3ab2c90f8459a83616391e7f8fc
Eliuds <cmeliud2003@gmail.com>
2022-04-26 09:39:12 -0400

    asd

--- OOP SUBMISSION ASSIGNMENT/OOP SUBMISSION ASSIGNMENT/Source.cpp
+++ OOP SUBMISSION ASSIGNMENT/OOP SUBMISSION ASSIGNMENT/Source.cpp
@@ -2,39 +2,6 @@
 #include <string>
 using namespace std;
 
-//class dog {// assignment 1
-//public:
-//	string breed;
-//	string color;
-//	string height;
-//	string weight;
-//	string behavior = "Dogs shake, sit and lay down.";
-//
-//};
-//
-//int main()
-//{
-//	dog dog1;
-//
-//	dog1.breed = "Hound";
-//	dog1.color = "Brown";
-//	dog1.height = "2 feet";
-//	dog1.weight = "60 ibs";
-//	dog1.behavior = "The hounds shake, sit and lay down";
-//
-//
-//	cout << dog1.breed << "\n";
-//	cout << dog1.color << "\n";
-//	cout << dog1.height << "\n";
-//	cout << dog1.weight << "\n";
-//	cout << dog1.behavior;
-//}
-
-
-
-
-
-
 class shape {
 public:
 	string color;
@@ -63,12 +30,13 @@ public:
 	string height;
 };
 
-int main() {
+int maintwo() {
 	shape2 triangle;
 	triangle.base;
 	triangle.color;
 	triangle.getArea;
 	triangle.height;
+	return 0;
 
 }
 class shape3 : public shape
@@ -77,11 +45,12 @@ public:
 	string radius;
 };
 
-int main() {
+int mainthree() {
 	shape3 circle;
 	circle.color;
 	circle.getArea;
 	circle.radius;
+	return 0;
 
 }
 
